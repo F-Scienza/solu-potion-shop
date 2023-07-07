3d66e16b2e8047bb9d0246ef5a9f6cf51b630b04
vadiun-dev <blanco.lucas.manuel@gmail.com>
2023-07-07 13:32:43 -0300

    changes

--- src/components/CarritoComponent.tsx
+++ src/components/CarritoComponent.tsx
@@ -1,54 +1,49 @@
-import { useContext } from 'react';
-import { PotionShopContext } from '../Context/AppContext';
-import CheckoutCard from './CheckoutCard';
-import { Product } from '../Types';
+import { useCarrito } from "../Context/AppContext";
+import { Product } from "../Types";
+import CheckoutCard from "./CheckoutCard";
 
 export const CarritoComponent = () => {
-	const context = useContext(PotionShopContext);
-	const { showCheckout, setShowCheckout, carrito, handleCompra } = context ?? {
-		showCheckout: false,
-		setShowCheckout: Boolean,
-		carrito: [],
-	};
+  const context = useCarrito();
+  const { showCheckout, setShowCheckout, carrito, handleCompra } = context;
 
-	const handleCheckout = () => setShowCheckout(!showCheckout);
+  const handleCheckout = () => setShowCheckout(!showCheckout);
 
-	return (
-		<aside
-			className={`${
-				showCheckout ? 'flex' : 'hidden'
-			} checkout-detail scrollable-cards flex-col items-center fixed right-4 border border-black rounded-lg bg-black/40 h-4/5 z-20 w-60`}
-		>
-			<h2 className="text-lg font-semibold mt-4">Carrito</h2>
-			<div className="flex justify-between p-4">
-				<span
-					onClick={handleCheckout}
-					className="cursor-pointer right-3 top-3 absolute"
-				>
-					x
-				</span>
-			</div>
-			<div className="px-2 ">
-				{carrito.map((prod: Product) => (
-					<CheckoutCard
-						key={prod.id}
-						id={prod.id}
-						nombre={prod.nombre}
-						imagen={prod.imagen}
-						precio={prod.precio}
-						categoria={prod.categoria}
-						descripcion={prod.descripcion}
-					/>
-				))}
-			</div>
-			<button
-				onClick={handleCompra}
-				className="relative inline-flex items-center justify-center p-0.5  mb-2 mr-2 overflow-hidden text-sm font-medium text-gray-900 rounded-lg group bg-gradient-to-br from-pink-600 to-orange-700 group-hover:from-pink-500 group-hover:to-orange-400 hover:text-white dark:text-white focus:ring-4 focus:outline-none focus:ring-pink-200 dark:focus:ring-pink-900"
-			>
-				<span className="relative px-5 py-1 transition-all ease-in duration-75 bg-white dark:bg-gray-900 rounded-md group-hover:bg-opacity-0">
-					Comprar
-				</span>
-			</button>
-		</aside>
-	);
+  return (
+    <aside
+      className={`${
+        showCheckout ? "flex" : "hidden"
+      } checkout-detail scrollable-cards flex-col items-center fixed right-4 border border-black rounded-lg bg-black/40 h-4/5 z-20 w-60`}
+    >
+      <h2 className="text-lg font-semibold mt-4">Carrito</h2>
+      <div className="flex justify-between p-4">
+        <span
+          onClick={handleCheckout}
+          className="cursor-pointer right-3 top-3 absolute"
+        >
+          x
+        </span>
+      </div>
+      <div className="px-2 ">
+        {carrito.map((prod: Product) => (
+          <CheckoutCard
+            key={prod.id}
+            id={prod.id}
+            nombre={prod.nombre}
+            imagen={prod.imagen}
+            precio={prod.precio}
+            categoria={prod.categoria}
+            descripcion={prod.descripcion}
+          />
+        ))}
+      </div>
+      <button
+        onClick={handleCompra}
+        className="relative inline-flex items-center justify-center p-0.5  mb-2 mr-2 overflow-hidden text-sm font-medium text-gray-900 rounded-lg group bg-gradient-to-br from-pink-600 to-orange-700 group-hover:from-pink-500 group-hover:to-orange-400 hover:text-white dark:text-white focus:ring-4 focus:outline-none focus:ring-pink-200 dark:focus:ring-pink-900"
+      >
+        <span className="relative px-5 py-1 transition-all ease-in duration-75 bg-white dark:bg-gray-900 rounded-md group-hover:bg-opacity-0">
+          Comprar
+        </span>
+      </button>
+    </aside>
+  );
 };
--- src/components/HeaderComponent.tsx
+++ src/components/HeaderComponent.tsx
@@ -1,25 +1,24 @@
-import { useContext } from 'react';
-import { PotionShopContext } from '../Context/AppContext';
+import { useCarrito } from "../Context/AppContext";
 
 export const HeaderComponent = () => {
-	const context = useContext(PotionShopContext);
-	const { billetera, carrito, setShowCheckout, showCheckout } = context ?? { billetera: 3, setShowCheckout: Boolean };
+  const context = useCarrito();
+  const { billetera, carrito, setShowCheckout, showCheckout } = context;
 
-	const handleShowCheckout = () => setShowCheckout(!showCheckout);
+  const handleShowCheckout = () => setShowCheckout(!showCheckout);
 
-	return (
-		<div className="bg-gradient-to-b from-black/80 to-black/30 py-4 px-8 flex justify-between items-center sticky top-0 shadow-md z-10">
-			<h1 className="text-white text-xl font-bold">🧙‍♂️ Potion Shop</h1>
-			<div className="flex gap-2 items-center">
-				<img src="./gem.png" alt="gema" className="w-12" />
-				<span>{billetera} Gemas</span>
-			</div>
-			<button
-				className="text-white hover:underline"
-				onClick={handleShowCheckout}
-			>
-				{showCheckout ? 'Cerrar carrito' : `Ver carrito ${carrito?.length}`}
-			</button>
-		</div>
-	);
+  return (
+    <div className="bg-gradient-to-b from-black/80 to-black/30 py-4 px-8 flex justify-between items-center sticky top-0 shadow-md z-10">
+      <h1 className="text-white text-xl font-bold">🧙‍♂️ Potion Shop</h1>
+      <div className="flex gap-2 items-center">
+        <img src="./gem.png" alt="gema" className="w-12" />
+        <span>{billetera} Gemas</span>
+      </div>
+      <button
+        className="text-white hover:underline"
+        onClick={handleShowCheckout}
+      >
+        {showCheckout ? "Cerrar carrito" : `Ver carrito ${carrito?.length}`}
+      </button>
+    </div>
+  );
 };
